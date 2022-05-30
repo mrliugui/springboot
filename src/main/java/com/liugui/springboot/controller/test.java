3a5eb779f3bac402c60a364c455b92f5061019a8
mrliugui <2095771434@qq.com>
2022-05-30 21:53:40 +0800

    关于yml，以及自定义properties的使用

--- src/main/java/com/liugui/springboot/controller/test.java
+++ src/main/java/com/liugui/springboot/controller/test.java
@@ -1,12 +1,17 @@
 package com.liugui.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
+//    可以使用默认值利用冒号隔开
+    @Value("${name:liugui}")
+    private String name1;
     @PostMapping("/test")
     public String test(String name){
-        return "hello world"+name;
+        System.out.println(name1);
+        return "hello world"+name+"  "+name1;
     }
 }
