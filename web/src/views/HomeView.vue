3ec8cd549e67070d6e807b02872b3fd11408e974
mrliugui <2095771434@qq.com>
2022-05-31 23:05:38 +0800

    引入antdvUi，用来书写网页显示页面

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
+    <a-button danger>Danger Default</a-button>
+    <a-button danger disabled>Danger Default(disabled)</a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {defineComponent} from 'vue';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default defineComponent({
+    export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
