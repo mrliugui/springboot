ef3056237a4b2c42108d933bf212008e932b8015
mrliugui <2095771434@qq.com>
2022-05-31 16:34:44 +0800

    对于copyProperties的进一步封装，提高程序的移植性

--- src/main/java/com/liugui/springboot/service/serviceImpl/EbookServiceImpl.java
+++ src/main/java/com/liugui/springboot/service/serviceImpl/EbookServiceImpl.java
@@ -3,12 +3,11 @@ package com.liugui.springboot.service.serviceImpl;
 import com.liugui.springboot.dao.EbookMapper;
 import com.liugui.springboot.pojo.Ebook;
 import com.liugui.springboot.service.EbookService;
+import com.liugui.springboot.util.CopyUtil;
 import com.liugui.springboot.vo.EbookVo;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,13 +19,14 @@ public class EbookServiceImpl implements EbookService {
     @Override
     public List<EbookVo> bookList(String name)
     {
-        List<EbookVo> ebookVos = new ArrayList<>();
+//        List<EbookVo> ebookVos = new ArrayList<>();
         List<Ebook> ebooks = ebookMapper.searchLike(name);
-        for (Ebook ebook : ebooks) {
-            EbookVo ebookVo = new EbookVo();
-            BeanUtils.copyProperties(ebook,ebookVo);
-            ebookVos.add(ebookVo);
-        }
+//        for (Ebook ebook : ebooks) {
+//            EbookVo ebookVo = new EbookVo();
+//            BeanUtils.copyProperties(ebook,ebookVo);
+//            ebookVos.add(ebookVo);
+//        }
+        List<EbookVo> ebookVos=CopyUtil.copyList(ebooks,EbookVo.class);
         return ebookVos;
     }
 }
