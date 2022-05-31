03941d05b2b1d2f8d8e3831abf0f1db04d7e8f45
mrliugui <2095771434@qq.com>
2022-05-31 11:18:12 +0800

    关于mybatis-generator集成spring boot ，以及xml书写

---
+++ src/main/java/com/liugui/springboot/dao/DetailMapper.java
@@ -0,0 +1,17 @@
+package com.liugui.springboot.dao;
+
+import com.liugui.springboot.pojo.Detail;
+
+public interface DetailMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Detail record);
+
+    int insertSelective(Detail record);
+
+    Detail selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Detail record);
+
+    int updateByPrimaryKey(Detail record);
+}
