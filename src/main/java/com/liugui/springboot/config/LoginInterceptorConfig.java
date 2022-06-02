1b1f66cd04b8b9f62eb052bf604753ba3d9b3ce3
mrliugui <2095771434@qq.com>
2022-06-02 10:02:32 +0800

    关于Interceptor的书写，以及配置。

---
+++ src/main/java/com/liugui/springboot/config/LoginInterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.liugui.springboot.config;
+
+import com.liugui.springboot.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class LoginInterceptorConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+
+    }
+
+}
